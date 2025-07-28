5b5fa6d9536414657d8475f551cd9a4dfff65136
Bandari Sandeep <107421940+Bandarisandeep26@users.noreply.github.com>
2025-07-28 23:52:16 +0530

    Update main.tf
    add random string

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,11 @@
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "aws_s3_bucket" "this" {
-  count  = var.bucket_count
-  bucket = "${var.bucket_name_prefix}-${count.index + 1}"
-  tags   = var.tags
+  count = var.bucket_count
+  bucket = "${var.bucket_name_prefix}-${count.index + 1}-${random_string.suffix.result}"
+  force_destroy = true
 }
