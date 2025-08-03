974b3e90029437e5a8271dc65aec92d9a4f3eda5
Bandari Sandeep <107421940+Bandarisandeep26@users.noreply.github.com>
2025-08-03 23:34:55 +0530

    Update main.tf
    add subnets for vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,3 +7,16 @@ resource "aws_vpc" "this" {
     Name = var.name
   }
 }
+
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
