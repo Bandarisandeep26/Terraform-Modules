f745e3910785179eb5077402cbd7497d5cf8ed9f
Bandari Sandeep <107421940+Bandarisandeep26@users.noreply.github.com>
2025-08-03 22:46:39 +0530

    Create variables.tf
    add variables.tf in sg

---
+++ modules/security-group/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {}
+variable "description" {}
+variable "vpc_id" {}
+
+variable "ingress_rules" {
+  type = list(object({
+    description = optional(string)
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  type = list(object({
+    description = optional(string)
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
