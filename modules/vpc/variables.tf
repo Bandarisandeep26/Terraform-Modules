018153489d21e6b7cc92ce4ea11caa3380bd23f9
Bandari Sandeep <107421940+Bandarisandeep26@users.noreply.github.com>
2025-08-03 23:37:27 +0530

    Update variables.tf
    add variables for subnets

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,19 @@
-variable "cidr_block" {
+variable "vpc_cidr" {
   type        = string
   description = "CIDR block for the VPC"
 }
 
 variable "name" {
   type        = string
-  description = "Name tag for the VPC"
+  description = "Name of the VPC"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDR blocks for public subnets"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "List of availability zones"
 }
